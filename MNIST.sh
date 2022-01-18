9a1cfba4b393cd87448eea3c8982d75da7e202a6
ma3oun <berjaoui@gmail.com>
2022-01-18 16:40:54 +0100

    First working version (but still some cleaning up to do)

--- MNIST.sh
+++ MNIST.sh
@@ -1,56 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+export DATASETS_ROOT="/mnt/nas/raid0/workspace/datasets/wip"
 
-CUDA_VISIBLE_DEVICES=0 python3 mnist.py 0 0 & 
-CUDA_VISIBLE_DEVICES=1 python3 mnist.py 1 0 & 
-CUDA_VISIBLE_DEVICES=2 python3 mnist.py 2 0 & 
-CUDA_VISIBLE_DEVICES=3 python3 mnist.py 3 0 & 
-CUDA_VISIBLE_DEVICES=4 python3 mnist.py 4 0 & 
-CUDA_VISIBLE_DEVICES=5 python3 mnist.py 5 0 & 
-CUDA_VISIBLE_DEVICES=6 python3 mnist.py 6 0 & 
-CUDA_VISIBLE_DEVICES=7 python3 mnist.py 7 0
-
-
-sleep 5
-CUDA_VISIBLE_DEVICES=0 python3 mnist.py 0 1 & 
-CUDA_VISIBLE_DEVICES=1 python3 mnist.py 1 1 & 
-CUDA_VISIBLE_DEVICES=2 python3 mnist.py 2 1 & 
-CUDA_VISIBLE_DEVICES=3 python3 mnist.py 3 1 & 
-CUDA_VISIBLE_DEVICES=4 python3 mnist.py 4 1 & 
-CUDA_VISIBLE_DEVICES=5 python3 mnist.py 5 1 & 
-CUDA_VISIBLE_DEVICES=6 python3 mnist.py 6 1 & 
-CUDA_VISIBLE_DEVICES=7 python3 mnist.py 7 1
-
-
-sleep 5
-CUDA_VISIBLE_DEVICES=0 python3 mnist.py 0 2 & 
-CUDA_VISIBLE_DEVICES=1 python3 mnist.py 1 2 & 
-CUDA_VISIBLE_DEVICES=2 python3 mnist.py 2 2 & 
-CUDA_VISIBLE_DEVICES=3 python3 mnist.py 3 2 & 
-CUDA_VISIBLE_DEVICES=4 python3 mnist.py 4 2 & 
-CUDA_VISIBLE_DEVICES=5 python3 mnist.py 5 2 & 
-CUDA_VISIBLE_DEVICES=6 python3 mnist.py 6 2 & 
-CUDA_VISIBLE_DEVICES=7 python3 mnist.py 7 2
-
-
-sleep 5
-CUDA_VISIBLE_DEVICES=0 python3 mnist.py 0 3 & 
-CUDA_VISIBLE_DEVICES=1 python3 mnist.py 1 3 & 
-CUDA_VISIBLE_DEVICES=2 python3 mnist.py 2 3 & 
-CUDA_VISIBLE_DEVICES=3 python3 mnist.py 3 3 & 
-CUDA_VISIBLE_DEVICES=4 python3 mnist.py 4 3 & 
-CUDA_VISIBLE_DEVICES=5 python3 mnist.py 5 3 & 
-CUDA_VISIBLE_DEVICES=6 python3 mnist.py 6 3 & 
-CUDA_VISIBLE_DEVICES=7 python3 mnist.py 7 3
-
-
-sleep 5
-CUDA_VISIBLE_DEVICES=0 python3 mnist.py 0 4 & 
-CUDA_VISIBLE_DEVICES=1 python3 mnist.py 1 4 & 
-CUDA_VISIBLE_DEVICES=2 python3 mnist.py 2 4 & 
-CUDA_VISIBLE_DEVICES=3 python3 mnist.py 3 4 & 
-CUDA_VISIBLE_DEVICES=4 python3 mnist.py 4 4 & 
-CUDA_VISIBLE_DEVICES=5 python3 mnist.py 5 4 & 
-CUDA_VISIBLE_DEVICES=6 python3 mnist.py 6 4 &
-CUDA_VISIBLE_DEVICES=7 python3 mnist.py 7 4
-
+for session in {0..4}
+do
+    for testCase in {0..4}
+    do
+        CUDA_VISIBLE_DEVICES=0 python mnist.py $((2*testCase)) $session &
+        CUDA_VISIBLE_DEVICES=1 python mnist.py $((2*testCase+1)) $session
+    done
+    
+    sleep 5
+done
