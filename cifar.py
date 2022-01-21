fe6821aec3568419bcb0d38c8786fbab96c4bc2a
ma3oun <berjaoui@gmail.com>
2022-01-21 15:36:43 +0100

    Changed to use cl_datasets

--- cifar.py
+++ cifar.py
@@ -1,59 +1,97 @@
-'''
+"""
 RPS network training on CIFAR100
 Copyright (c) Jathushan Rajasegaran, 2019
-'''
-from __future__ import print_function
+"""
 
-import argparse
 import os
-import shutil
 import time
 import random
-import pickle
 import torch
-import pdb
-import torch.nn as nn
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.optim as optim
-import torch.utils.data as data
 import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
+from utils import mkdir_p
 
 import numpy as np
 import copy
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.autograd import Variable
-from torch.autograd import gradcheck
 import sys
 import random
-
+from torch.utils.data import TensorDataset
 
 from rps_net import RPS_net_cifar
 from learner import Learner
 from util import *
-from cifar_dataset import CIFAR100
+
+
+def load_cifar(
+    batchSize: int, currentSession: int, replayBufferSize: int, nClsPerTask: int
+):
+    from cl_datasets import getDatasets
+
+    trainTransforms = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
+
+    testTransforms = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
+
+    if currentSession == 0:
+        train_loader, test_loader = getDatasets(
+            f"cifar100_{currentSession}",
+            batchSize,
+            trainTransforms=trainTransforms,
+            testTransforms=testTransforms,
+        )
+    else:
+        # The replay buffer needs to filled
+        nElementsPerTask = replayBufferSize // nClsPerTask
+        dataBuffer = []
+        for previousSession in range(currentSession):
+            previousLoader, _ = getDatasets(
+                f"cifar100_{previousSession}",
+                1,
+                trainTransforms=trainTransforms,
+                testTransforms=testTransforms,
+            )
+            dataBuffer.extend(
+                [(x.squeeze(0), y.squeeze()) for x, y in previousLoader][
+                    :nElementsPerTask
+                ]
+            )
+
+        xTensor = torch.stack([x for x, _ in dataBuffer])
+        yTensor = torch.stack([y for _, y in dataBuffer])
+        extraDataset = TensorDataset(xTensor, yTensor)
+
+        train_loader, test_loader = getDatasets(
+            f"cifar100_{currentSession}",
+            batchSize,
+            trainTransforms=trainTransforms,
+            testTransforms=testTransforms,
+            extraTrainingData=extraDataset,
+        )
+
+    return train_loader, test_loader
 
 
 class args:
-
-    checkpoint = "results/cifar100/RPS_CIFAR_M8_J1"
-    labels_data = "prepare/cifar100_10.pkl"
+    epochs = 100
+    checkpoint = "results/cifar100/RPS_net_cifar"
     savepoint = ""
-    
+    dataset = "cifar-100"
     num_class = 100
     class_per_task = 10
     M = 8
-    jump = 2
-    rigidness_coff = 2.5
-    dataset = "CIFAR"
-    
-    epochs = 100
-    L = 9
+    nLayers = 9
     N = 1
     lr = 0.001
     train_batch = 128
@@ -67,9 +105,15 @@ class args:
     test_case = 0
     schedule = [20, 40, 60, 80]
     gamma = 0.5
+    rigidness_coff = 2.5
+    jump = 2
 
-    
-state = {key:value for key, value in args.__dict__.items() if not key.startswith('__') and not callable(key)}
+
+state = {
+    key: value
+    for key, value in args.__dict__.items()
+    if not key.startswith("__") and not callable(key)
+}
 print(state)
 
 # Use CUDA
@@ -81,139 +125,140 @@ if use_cuda:
     torch.cuda.manual_seed_all(seed)
 
 
-
-
 def main():
-
-
-    model = RPS_net_cifar(args).cuda() 
-    print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
-
-    
-    
     if not os.path.isdir(args.checkpoint):
         mkdir_p(args.checkpoint)
-        
-    if not os.path.isdir("models/CIFAR100/"+args.checkpoint.split("/")[-1]):
-        mkdir_p("models/CIFAR100/"+args.checkpoint.split("/")[-1])
-    args.savepoint = "models/CIFAR100/"+args.checkpoint.split("/")[-1]
 
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(10),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
+    if not os.path.isdir("models/CIFAR100/" + args.checkpoint.split("/")[-1]):
+        mkdir_p("models/CIFAR100/" + args.checkpoint.split("/")[-1])
+    args.savepoint = "models/CIFAR100/" + args.checkpoint.split("/")[-1]
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
+    model = RPS_net_cifar(args.M)
+    print(model)
 
-    dataloader = CIFAR100
-    
-    
-    
-    
-    start_sess = int(sys.argv[2])
+    current_sess = int(sys.argv[2])
     test_case = sys.argv[1]
     args.test_case = test_case
 
-    inds_all_sessions=pickle.load(open(args.labels_data,'rb'))
+    memory = 2000
 
-        
-    for ses in range(start_sess, start_sess+1):
-        if(ses==0):
-            path = get_path(args.L,args.M,args.N)*0 
-            path[:,0] = 1
-            fixed_path = get_path(args.L,args.M,args.N)*0 
-            train_path = path.copy()
-            infer_path = path.copy()
+    train_loader, test_loader = load_cifar(
+        args.train_batch, current_sess, memory, args.class_per_task
+    )
+
+    if current_sess == 0:
+        # First session
+        path = np.zeros((args.nLayers, args.M), dtype=bool)
+        path[:, 0] = True
+        fixed_path = np.zeros((args.nLayers, args.M), dtype=bool)
+        train_path = path.copy()
+        infer_path = path.copy()
+    else:
+        load_test_case = get_best_model(current_sess - 1, args.checkpoint)
+        if current_sess % args.jump == 0:
+            # Get a new path
+            fixed_path = np.load(
+                os.path.join(
+                    args.checkpoint,
+                    f"fixed_path_{current_sess -1}_{load_test_case}.npy",
+                )
+            )
+
+            path = get_path(args.nLayers, args.M, args.N)
+            train_path = np.zeros((args.nLayers, args.M), dtype=bool)
         else:
-            load_test_case = get_best_model(ses-1, args.checkpoint)
-            if(ses%args.jump==0):   #get a new path
-                fixed_path = np.load(args.checkpoint+"/fixed_path_"+str(ses-1)+"_"+str(load_test_case)+".npy")
-                path = get_path(args.L,args.M,args.N)
-                train_path = get_path(args.L,args.M,args.N)*0 
+            if current_sess // args.jump == 0:
+                # This is the first jump
+                fixed_path = np.zeros((args.nLayers, args.M), dtype=bool)
             else:
-                if((ses//args.jump)==0):
-                    fixed_path = get_path(args.L,args.M,args.N)*0
-                else:
-                    load_test_case_x = get_best_model((ses//args.jump)*args.jump-1, args.checkpoint)
-                    fixed_path = np.load(args.checkpoint+"/fixed_path_"+str((ses//args.jump)*args.jump-1)+"_"+str(load_test_case_x)+".npy")
-                path = np.load(args.checkpoint+"/path_"+str(ses-1)+"_"+str(load_test_case)+".npy")
-                train_path = get_path(args.L,args.M,args.N)*0 
-            infer_path = get_path(args.L,args.M,args.N)*0 
-            for j in range(args.L):
-                for i in range(args.M):
-                    if(fixed_path[j,i]==0 and path[j,i]==1):
-                        train_path[j,i]=1
-                    if(fixed_path[j,i]==1 or path[j,i]==1):
-                        infer_path[j,i]=1
-            
-        np.save(args.checkpoint+"/path_"+str(ses)+"_"+str(test_case)+".npy", path)
-        
-        
-        print('Starting with session {:d}'.format(ses))
-        print('test case : ' + str(test_case))
-        print('#################################################################################')
-        print("path\n",path)
-        print("fixed_path\n",fixed_path)
-        print("train_path\n", train_path)
-        
-    
-        ind_this_session=inds_all_sessions[ses]    
-        ind_trn= ind_this_session['curent']
-        if ses > 0: ind_trn = np.concatenate([ind_trn,  np.tile(inds_all_sessions[ses-1]['exmp'],int(1))]).ravel()
-        ind_tst=inds_all_sessions[ses]['test']
+                # Get data from the last jump
+                lastJump = (current_sess // args.jump) * args.jump - 1
+                load_test_case_x = get_best_model(lastJump, args.checkpoint)
+                fixed_path = np.load(
+                    os.path.join(
+                        args.checkpoint,
+                        f"fixed_path_{lastJump}_{load_test_case_x}.npy",
+                    )
+                )
+            path = np.load(
+                os.path.join(
+                    args.checkpoint, f"path_{current_sess-1}_{load_test_case}.npy"
+                )
+            )
+            train_path = np.zeros((args.nLayers, args.M), dtype=bool)
+        infer_path = np.zeros((args.nLayers, args.M), dtype=bool)
+        train_path = ~fixed_path & path
+        infer_path = fixed_path | path
 
-        
-        trainset = dataloader(root='./data', train=True, download=True, transform=transform_train,ind=ind_trn)
-        trainloader = data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True,num_workers=args.workers)
-        testset = dataloader(root='./data', train=False, download=False, transform=transform_test,ind=ind_tst)
-        testloader = data.DataLoader(testset, batch_size=args.test_batch, shuffle=False, num_workers=args.workers)
-        
-        
-        args.sess=ses      
-        if ses>0: 
-            path_model=os.path.join(args.savepoint, 'session_'+str(ses-1)+'_'+str(load_test_case)+'_model_best.pth.tar')
-            prev_best=torch.load(path_model)
-            model.load_state_dict(prev_best['state_dict'])
+    np.save(os.path.join(args.checkpoint, f"path_{current_sess}_{test_case}.npy"), path)
+    if current_sess == 0:
+        fixed_path_x = path.copy()
+    else:
+        fixed_path_x = ~fixed_path & path
+    np.save(
+        os.path.join(args.checkpoint, f"fixed_path_{current_sess}_{test_case}.npy"),
+        fixed_path_x,
+    )
 
+    print(f"Starting with session {current_sess}")
+    print(f"test case : {test_case}")
+    print("#" * 80)
+    print(f"path\n{path}")
+    print(f"fixed_path\n{fixed_path}")
+    print(f"train_path\n{train_path}")
+    print(f"infer_path\n{infer_path}")
 
-        main_learner=Learner(model=model,args=args,trainloader=trainloader,
-                             testloader=testloader,old_model=copy.deepcopy(model),
-                             use_cuda=use_cuda, path=path, 
-                             fixed_path=fixed_path, train_path=train_path, infer_path=infer_path)
-        main_learner.learn()
+    args.sess = current_sess
 
-        
-        if(ses==0):
-            fixed_path = path.copy()
-        else:
-            for j in range(args.L):
-                for i in range(args.M):
-                    if(fixed_path[j,i]==0 and path[j,i]==1):
-                        fixed_path[j,i]=1
-        np.save(args.checkpoint+"/fixed_path_"+str(ses)+"_"+str(test_case)+".npy", fixed_path)
-        
-        
-        best_model = get_best_model(ses, args.checkpoint)
-    
-        cfmat = main_learner.get_confusion_matrix(infer_path)
-        np.save(args.checkpoint+"/confusion_matrix_"+str(ses)+"_"+str(test_case)+".npy", cfmat)
-        
-        
-    print('done with session {:d}'.format(ses))
-    print('#################################################################################')
-    while(1):
-        if(is_all_done(ses, args.epochs, args.checkpoint)):
+    if current_sess > 0:
+        path_model = os.path.join(
+            args.savepoint,
+            f"session_{current_sess - 1}_{load_test_case}_model_best.pth.tar",
+        )
+        prev_best = torch.load(path_model)
+        model.load_state_dict(prev_best["state_dict"])
+
+    main_learner = Learner(
+        model=model,
+        args=args,
+        trainloader=train_loader,
+        testloader=test_loader,
+        old_model=copy.deepcopy(model),
+        use_cuda=use_cuda,
+        path=path,
+        fixed_path=fixed_path,
+        train_path=train_path,
+        infer_path=infer_path,
+        title="cifar100",
+    )
+    main_learner.learn()
+
+    if current_sess == 0:
+        fixed_path = path.copy()
+    else:
+        fixed_path = ~fixed_path & path
+
+    np.save(
+        os.path.join(args.checkpoint, f"fixed_path_{current_sess}_{test_case}.npy"),
+        fixed_path,
+    )
+
+    cfmat = main_learner.get_confusion_matrix(infer_path)
+    np.save(
+        os.path.join(
+            args.checkpoint, f"confusion_matrix_{current_sess}_{test_case}.npy"
+        ),
+        cfmat,
+    )
+
+    print(f"done with session {current_sess}")
+    print("#" * 80)
+    while True:
+        if is_all_done(current_sess, args.epochs, args.checkpoint):
             break
         else:
             time.sleep(10)
-            
-            
-            
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
