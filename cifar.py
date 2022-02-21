a838605818918b33ccc0ce5df6afa8f169822495
ma3oun <berjaoui@gmail.com>
2022-02-21 10:13:07 +0100

    Started using easydict

--- cifar.py
+++ cifar.py
@@ -7,45 +7,42 @@ import sys
 from runner import main
 from rps_net import RPS_net_cifar
 from datasets import cifar100Dataset
+from easydict import EasyDict
+
+params = EasyDict()
 
 
-class args:
-    datasetName = "cifar100"
-    epochs = 100
-    checkpoint = "results/cifar100/RPS_net_cifar"
-    savepoint = ""
-    dataset = "cifar-100"
-    num_class = 100
-    class_per_task = 10
-    M = 8
-    nLayers = 9
-    N = 1
-    lr = 0.001
-    batchSize = 128
-    workers = 16
-    resume = False  # otherwise: indicate path to checkpoint file
-    arch = "res-18"
-    start_epoch = 0
-    evaluate = False
-    sess = 0
-    test_case = 0
-    schedule = [20, 40, 60, 80]
-    gamma = 0.5
-    rigidness_coff = 2.5
-    jump = 2
-    memory = 2000
+params.datasetName = "cifar100"
+params.epochs = 100
+params.checkpoint = "results/cifar100/RPS_net_cifar"
+params.savepoint = ""
+params.dataset = "cifar-100"
+params.num_class = 100
+params.class_per_task = 10
+params.M = 8
+params.nLayers = 9
+params.N = 1
+params.lr = 0.001
+params.batchSize = 128
+params.workers = 16
+params.resume = False  # otherwise: indicate path to checkpoint file
+params.arch = "res-18"
+params.start_epoch = 0
+params.evaluate = False
+params.sess = 0
+params.test_case = 0
+params.schedule = [20, 40, 60, 80]
+params.gamma = 0.5
+params.rigidness_coff = 2.5
+params.jump = 2
+params.memory = 2000
 
 
 if __name__ == "__main__":
-    state = {
-        key: value
-        for key, value in args.__dict__.items()
-        if not key.startswith("__") and not callable(key)
-    }
-    print(state)
-    model = RPS_net_cifar(args.M)
+    print(params)
+    model = RPS_net_cifar(params.M)
     print(model)
 
     current_sess = int(sys.argv[2])
     test_case = sys.argv[1]
-    main(args, model, cifar100Dataset, test_case, current_sess)
+    main(params, model, cifar100Dataset, test_case, current_sess)
--- mnist.py
+++ mnist.py
@@ -7,45 +7,43 @@ import sys
 from runner import main
 from rps_net import RPS_net_mlp
 from datasets import mnistDataset
+from easydict import EasyDict
 
 
-class args:
-    datasetName = "mnist"
-    epochs = 10
-    checkpoint = "results/mnist/RPS_net_mnist"
-    savepoint = "results/mnist/pathnet_mnist"
-    dataset = "MNIST"
-    num_class = 10
-    class_per_task = 2
-    M = 8
-    nLayers = 2
-    N = 1
-    lr = 0.001
-    batchSize = 128
-    workers = 16
-    resume = False
-    arch = "mlp"
-    start_epoch = 0
-    evaluate = False
-    sess = 0
-    test_case = 0
-    schedule = [6, 8, 16]
-    gamma = 0.5
-    rigidness_coff = 2.5
-    jump = 1
-    memory = 4400
+params = EasyDict()
+
+
+params.datasetName = "mnist"
+params.epochs = 10
+params.checkpoint = "results/mnist/RPS_net_mnist"
+params.savepoint = "results/mnist/pathnet_mnist"
+params.dataset = "MNIST"
+params.num_class = 10
+params.class_per_task = 2
+params.M = 8
+params.nLayers = 2
+params.N = 1
+params.lr = 0.001
+params.batchSize = 128
+params.workers = 16
+params.resume = False
+params.arch = "mlp"
+params.start_epoch = 0
+params.evaluate = False
+params.sess = 0
+params.test_case = 0
+params.schedule = [6, 8, 16]
+params.gamma = 0.5
+params.rigidness_coff = 2.5
+params.jump = 1
+params.memory = 4400
 
 
 if __name__ == "__main__":
-    state = {
-        key: value
-        for key, value in args.__dict__.items()
-        if not key.startswith("__") and not callable(key)
-    }
-    print(state)
-    model = RPS_net_mlp(args.M)
+    print(params)
+    model = RPS_net_mlp(params.M)
     print(model)
 
     current_sess = int(sys.argv[2])
     test_case = sys.argv[1]
-    main(args, model, mnistDataset, test_case, current_sess)
+    main(params, model, mnistDataset, test_case, current_sess)
