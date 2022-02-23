80f46f9b2883c155f827cbd517f5f06843431407
ThomasCloarec <thomas.cloarec.d@gmail.com>
2022-02-23 13:46:00 +0100

    Update multi_cifar for 8 GPUs

--- MULTI_CIFAR1.sh
+++ MULTI_CIFAR1.sh
@@ -10,10 +10,10 @@ trap 'jobs -p | xargs kill' SIGSTOP
 for session in {0..10}
 do
     echo "============== Task: $session =============="
-    for testCase in {0..3}
-    do
-        CUDA_VISIBLE_DEVICES=testCase python multi_cifar.py $((testCase)) $session &
-    done
+      CUDA_VISIBLE_DEVICES=0 python multi_cifar.py $((0)) $session &
+      CUDA_VISIBLE_DEVICES=1 python multi_cifar.py $((1)) $session &
+      CUDA_VISIBLE_DEVICES=2 python multi_cifar.py $((2)) $session &
+      CUDA_VISIBLE_DEVICES=3 python multi_cifar.py $((3)) $session
 
     sleep 5
 done
--- MULTI_CIFAR2.sh
+++ MULTI_CIFAR2.sh
@@ -10,10 +10,10 @@ trap 'jobs -p | xargs kill' SIGSTOP
 for session in {0..10}
 do
     echo "============== Task: $session =============="
-    for testCase in {0..3}
-    do
-        CUDA_VISIBLE_DEVICES=testCase python multi_cifar.py $((testCase+4)) $session &
-    done
+      CUDA_VISIBLE_DEVICES=4 python multi_cifar.py $((4)) $session &
+      CUDA_VISIBLE_DEVICES=5 python multi_cifar.py $((5)) $session &
+      CUDA_VISIBLE_DEVICES=6 python multi_cifar.py $((6)) $session &
+      CUDA_VISIBLE_DEVICES=7 python multi_cifar.py $((7)) $session
 
     sleep 5
 done
