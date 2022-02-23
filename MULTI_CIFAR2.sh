b6b4ccf6a58dd14682ba58809447ffd162cc54bb
ThomasCloarec <thomas.cloarec.d@gmail.com>
2022-02-23 13:47:16 +0100

    Update multi_cifar for 8 GPUs

--- MULTI_CIFAR2.sh
+++ MULTI_CIFAR2.sh
@@ -10,10 +10,10 @@ trap 'jobs -p | xargs kill' SIGSTOP
 for session in {0..10}
 do
     echo "============== Task: $session =============="
-      CUDA_VISIBLE_DEVICES=4 python multi_cifar.py $((4)) $session &
-      CUDA_VISIBLE_DEVICES=5 python multi_cifar.py $((5)) $session &
-      CUDA_VISIBLE_DEVICES=6 python multi_cifar.py $((6)) $session &
-      CUDA_VISIBLE_DEVICES=7 python multi_cifar.py $((7)) $session
+      CUDA_VISIBLE_DEVICES=0 python multi_cifar.py $((4)) $session &
+      CUDA_VISIBLE_DEVICES=1 python multi_cifar.py $((5)) $session &
+      CUDA_VISIBLE_DEVICES=2 python multi_cifar.py $((6)) $session &
+      CUDA_VISIBLE_DEVICES=3 python multi_cifar.py $((7)) $session
 
     sleep 5
 done
